c243a0d5bf96957592cca35f3c7e66c3ce3fe23a
Sama Alharbi <176575888+samaalharbi2@users.noreply.github.com>
2024-10-30 01:26:48 +0300

    Add files via upload

---
+++ Beginner/Rondom_list.py
@@ -0,0 +1,12 @@
+import random
+friends = ["alice","Bob","Charlie","David","Emanuel"]
+
+# 1 Option
+print(random.choice(friends))
+
+# 2nd Option
+random_index = random.randint(1,4)
+print(friends[random_index])
+
+
+
